1f10cdb907ab4157b31f17fcc63d64a1dcc91ac9
Jonathan Ellis <jbellis@datastax.com>
2023-07-03 10:31:32 -0500

    wiring up the Index button to do something, but it seems to be getting confused by the background.js code
    console output is
    Indexing history
    Indexing history with 100 items
    Unchecked lastError value: Error: Could not establish connection. Receiving end does not exist. background.js:1
    Uncaught (in promise) Error: Could not establish connection. Receiving end does not exist.
        <anonymous> moz-extension://b4dcea9d-1fb7-4c97-93a4-3c1040c06067/background.js:30
    background.js:30:22
    Unchecked lastError value: Error: Could not establish connection. Receiving end does not exist. background.js:1

--- src/popup.js
+++ src/popup.js
@@ -3,21 +3,43 @@ document.getElementById('search-history').addEventListener('click', async functi
     browser.windows.create({ url: service_url + "/search?user_id=" + data.user.id});
 });
 
-document.getElementById('index-history').addEventListener('click', function() {
-    browser.history.search({text: '', startTime: 0}).then((historyItems) => {
-        for (let item of historyItems) {
-            let url = item.url;
-            // Open a new tab and wait for it to load completely before moving on to the next URL
-            browser.tabs.create({ url: url }).then((tab) => {
-                browser.tabs.onUpdated.addListener(function listener (tabId, changeInfo) {
-                    if (tabId === tab.id && changeInfo.status == 'complete') {
-                        browser.tabs.onUpdated.removeListener(listener);
-                        // Close the tab
-                        browser.tabs.remove(tabId);
-                        // Move on to the next URL in history
-                    }
-                });
-            });
+document.getElementById('index-history').addEventListener('click', async function() {
+    console.log("Indexing history");
+    let historyItems = await browser.history.search({text: '', startTime: 0});
+    console.log("Indexing history with " + historyItems.length + " items");
+
+    // Create a queue to hold the history items
+    let queue = Array.from(historyItems);
+
+    // Create a new window and open the first 5 tabs
+    try {
+        let window = await browser.windows.create();
+        console.log("opening first 5 tabs");
+        for (let i = 0; i < Math.min(5, queue.length); i++) {
+            let url = queue.shift();
+            console.log("opening " + url);
+            openTab(url, window.id);
         }
-    });
+    } catch (err) {
+        console.error(err);
+    }
+
+    // Define the function for opening a tab and setting up the listener
+    function openTab(item, windowId) {
+        let url = item.url;
+        browser.tabs.create({ url: url, windowId: windowId }).then((tab) => {
+            browser.tabs.onUpdated.addListener(function listener (tabId, changeInfo) {
+                if (tabId === tab.id && changeInfo.status == 'complete') {
+                    browser.tabs.onUpdated.removeListener(listener);
+                    // Close the tab
+                    browser.tabs.remove(tabId);
+                    // Open the next URL in queue
+                    if (queue.length > 0) {
+                        openTab(queue.shift(), windowId);
+                    }
+                }
+            });
+        });
+    }
 });
+
