6387c244a6570e2594da6ea35e2424f4ebb7b1b6
Jonathan Ellis <jbellis@datastax.com>
2023-07-04 09:12:19 -0500

    - do processing in background.js using executeScript, it's simpler - save tab when it's closed or after 1m of inactivity

--- src/background.js
+++ src/background.js
@@ -25,20 +25,6 @@ checkUserId().catch(err => console.error(err));
 //
 // Completion listener
 //
-function sendMessageWithRetry(tab, message, retries = 3) {
-    console.log("Sending message to tab " + tab + ": " + tab.id)
-    browser.tabs.sendMessage(tab.id, message)
-        .catch(err => {
-            if (retries > 0) {
-                setTimeout(() => {
-                    sendMessageWithRetry(tab, message, retries - 1);
-                }, 100);
-            } else {
-                console.error("Unable to notify completion for " + tab.url + ": " + err);
-            }
-        });
-}
-
 function isSpecialUrl(url) {
     const specialProtocols = [
         'about:',
@@ -61,8 +47,84 @@ function isSpecialUrl(url) {
     return false;
 }
 
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && !isSpecialUrl(tab.url)) {
-        sendMessageWithRetry(tab, {action: "TabUpdated"});
+let tabContent = {};
+let inactivityTimers = {};
+
+function getTabTextWithRetry(tab, retries=3) {
+    browser.tabs.executeScript(tab.id, {
+        code: 'document.documentElement.innerText;'
+    }).then((results) => {
+        // results is an array of tab HTML content strings
+        if (results[0] === undefined) {
+            console.log("TR " + tab.url + " text is undefined");
+            return;
+        }
+        if (results[0] !== "") {
+            tabContent[tab.id] = {
+                text: results[0],
+                url: tab.url,
+                title: tab.title
+            };
+        }
+
+        if (inactivityTimers[tab.id]) {
+            clearTimeout(inactivityTimers[tab.id]);
+        }
+        inactivityTimers[tab.id] = setTimeout(() => {
+            console.log("TR timer saving " + tab.id)
+            saveTextFrom(tab.id);
+        }, 60000);  // 1 minute
+    }).catch(err => {
+        if (retries > 0) {
+            setTimeout(() => {
+                getTabTextWithRetry(tab, retries - 1);
+            }, 100);
+        } else {
+            console.error("TR unable to capture " + tab.url + ": " + err);
+        }
+    });
+}
+
+async function saveTextFrom(tabId) {
+    console.log("TR saving " + tabId);
+    let data = await browser.storage.local.get('user');
+    if (tabContent[tabId] === undefined) {
+        return;
     }
+
+    await fetch(service_url + '/save_if_article', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            url: tabContent[tabId].url,
+            title: tabContent[tabId].title,
+            text_content: tabContent[tabId].text,
+            user_id: data.user.id
+        })
+    })
+        .then(response => response.json())
+        .then(url_data => {
+            console.assert(url_data && url_data.saved !== undefined,
+                "Unexpected response " + url_data + " for url " + tabContent[tabId].url);
+            console.log("TR " + tabContent[tabId].url + " -> " + url_data.saved);
+        })
+        .catch(err => console.error("TR " + err));
+
+    console.log("TR save complete for " + tabId);
+    delete tabContent[tabId];
+    delete inactivityTimers[tabId];
+}
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && !isSpecialUrl(tab.url)) {
+        console.log("TR tab " + tabId + " complete")
+        getTabTextWithRetry(tab);
+    }
+});
+
+browser.tabs.onRemoved.addListener((tabId) => {
+    console.log("TR tab " + tabId + " removed")
+    saveTextFrom(tabId);
 });
