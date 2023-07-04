1a6e985ce361e9b75aeefd3e813266309f4617e5
Jonathan Ellis <jbellis@datastax.com>
2023-07-04 09:36:56 -0500

    save when host/path changes

--- src/background.js
+++ src/background.js
@@ -49,6 +49,7 @@ function isSpecialUrl(url) {
 
 let tabContent = {};
 let inactivityTimers = {};
+let savingPromises = {};
 
 function getTabTextWithRetry(tab, retries=3) {
     browser.tabs.executeScript(tab.id, {
@@ -60,11 +61,19 @@ function getTabTextWithRetry(tab, retries=3) {
             return;
         }
         if (results[0] !== "") {
-            tabContent[tab.id] = {
+            let urlObj = new URL(tab.url);
+            let newContent = {
                 text: results[0],
                 url: tab.url,
+                path: urlObj.host + urlObj.pathname, // This includes the server name
                 title: tab.title
             };
+            if (tabContent[tab.id] !== undefined && tabContent[tab.id].path !== newContent.path) {
+                console.log("TR " + tab.id + " path changed to " + newContent.path);
+                savingPromises[tab.id] = saveText(tabContent[tab.id]);
+            }
+            console.log("TR " + tab.id + " text updated for " + newContent.path)
+            tabContent[tab.id] = newContent;
         }
 
         if (inactivityTimers[tab.id]) {
@@ -72,7 +81,7 @@ function getTabTextWithRetry(tab, retries=3) {
         }
         inactivityTimers[tab.id] = setTimeout(() => {
             console.log("TR timer saving " + tab.id)
-            saveTextFrom(tab.id);
+            savingPromises[tab.id] = saveText(tabContent[tab.id]);
         }, 60000);  // 1 minute
     }).catch(err => {
         if (retries > 0) {
@@ -85,46 +94,56 @@ function getTabTextWithRetry(tab, retries=3) {
     });
 }
 
-async function saveTextFrom(tabId) {
-    console.log("TR saving " + tabId);
+async function saveText(tabContent) {
+    console.log("TR saving " + tabContent.url);
     let data = await browser.storage.local.get('user');
-    if (tabContent[tabId] === undefined) {
-        return;
-    }
 
-    await fetch(service_url + '/save_if_article', {
+    return fetch(service_url + '/save_if_article', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            url: tabContent[tabId].url,
-            title: tabContent[tabId].title,
-            text_content: tabContent[tabId].text,
+            url: tabContent.url,
+            title: tabContent.title,
+            text_content: tabContent.text,
             user_id: data.user.id
         })
     })
         .then(response => response.json())
         .then(url_data => {
             console.assert(url_data && url_data.saved !== undefined,
-                "Unexpected response " + url_data + " for url " + tabContent[tabId].url);
-            console.log("TR " + tabContent[tabId].url + " -> " + url_data.saved);
+                "Unexpected response " + url_data + " for url " + tabContent.url);
+            console.log("TR " + tabContent.url + " -> " + url_data.saved);
         })
-        .catch(err => console.error("TR " + err));
-
-    console.log("TR save complete for " + tabId);
-    delete tabContent[tabId];
-    delete inactivityTimers[tabId];
+        .catch(err => console.error("TR " + err))
+        .finally(() => {
+            console.log("TR save complete for " + tabContent.url);
+            delete tabContent[tabContent.url];
+            delete inactivityTimers[tabContent.url];
+            delete savingPromises[tabContent.url];
+        });
 }
 
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && !isSpecialUrl(tab.url)) {
-        console.log("TR tab " + tabId + " complete")
+    if (changeInfo.status !== 'complete' || isSpecialUrl(tab.url)) {
+        return;
+    }
+
+    // If a save operation is ongoing when the tab updates, we wait for it to finish
+    if (savingPromises[tabId]) {
+        savingPromises[tabId].then(() => getTabTextWithRetry(tab));
+    } else {
         getTabTextWithRetry(tab);
     }
 });
 
 browser.tabs.onRemoved.addListener((tabId) => {
     console.log("TR tab " + tabId + " removed")
-    saveTextFrom(tabId);
+    if (tabContent[tabId]) {
+        if (savingPromises[tabId]) {
+            return;
+        }
+        savingPromises[tabId] = saveText(tabContent[tabId]);
+    }
 });
