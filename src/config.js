befa2988593c49da3ad487c881b5ca1f9af2c5de
Jonathan Ellis <jbellis@datastax.com>
2023-07-05 11:17:54 -0500

    use https

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // update this in manifest.json as well
-service_url = "http://totalrecall.click"
+service_url = "https://totalrecall.click"
 // service_url = "http://192.168.1.83:5000"
 
 ext_name = "Total Recall"
