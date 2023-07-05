01ff2f476f7fcd2388a9d8df2101048223a3aa38
Jonathan Ellis <jbellis@datastax.com>
2023-07-04 22:03:51 -0500

    Revert "switch back to new window for history loading.  still broken"
    This reverts commit 5339f787e17814178dd8c25ef9a11fa4565d2275.

--- src/popup.js
+++ src/popup.js
@@ -10,30 +10,29 @@ document.getElementById('index-history').addEventListener('click', async functio
     // Create a queue to hold the history items
     let queue = Array.from(historyItems, item => item.url);
 
-    // Create a new window and open the first 5 tabs
+    // Open the first 5 tabs
     try {
-        let window = await browser.windows.create();
         console.log("opening first 5 tabs");
         for (let i = 0; i < Math.min(5, queue.length); i++) {
             let url = queue.shift();
             console.log("opening " + url);
-            openTab(url, window.id);
+            openTab(url);
         }
     } catch (err) {
-        console.error("Error opening history window: " + err);
+        console.error("Error opening tabs: " + err);
     }
 
     // Define the function for opening a tab and setting up the listener
-    function openTab(url, windowId) {
-        browser.tabs.create({ url: url, windowId: windowId }).then((tab) => {
+    function openTab(url) {
+        browser.tabs.create({ url: url }).then((tab) => {
             let listener = function (tabId, changeInfo) {
-                if (tabId === tab.id && changeInfo.status === 'complete') {
+                if (tabId === tab.id && changeInfo.status == 'complete') {
                     browser.tabs.onUpdated.removeListener(listener);
                     // Close the tab
                     browser.tabs.remove(tabId);
                     // Open the next URL in queue
                     if (queue.length > 0) {
-                        openTab(queue.shift(), windowId);
+                        openTab(queue.shift());
                     }
                 }
             };
