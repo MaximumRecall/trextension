c64f5f397af16690417a6e355397d95069b720e2
Jonathan Ellis <jbellis@datastax.com>
2023-07-09 13:16:53 +0200

    update popup with copy/set uuid

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // update this in manifest.json as well
 service_url = "https://totalrecall.click"
-// service_url = "http://192.168.1.83:5000"
+// service_url = "http://localhost:5000"
 
 ext_name = "Total Recall"
--- src/popup.js
+++ src/popup.js
@@ -7,3 +7,31 @@ document.getElementById('index-history').addEventListener('click', function() {
     // Send a message to the background script to start the indexing
     browser.runtime.sendMessage({action: "indexHistory"});
 });
+
+document.getElementById('copy-uuid').addEventListener('click', copyUUID);
+document.getElementById('set-uuid').addEventListener('click', setUUID);
+
+// Function to copy the UUID to the clipboard
+async function copyUUID() {
+    let data = await browser.storage.local.get('user');
+    if (data.user && data.user.id) {
+        try {
+            await navigator.clipboard.writeText(data.user.id);
+            document.getElementById('message').innerText = 'Copied!';
+            setTimeout(() => document.getElementById('message').innerText = '', 2000); // Remove the message after 2 seconds
+        } catch (err) {
+            console.error('Failed to copy text: ', err);
+        }
+    }
+}
+
+// Function to set a new UUID
+async function setUUID() {
+    let newUUID = prompt("Please enter a new UUID:");
+    if (newUUID) {
+        let user = { id: newUUID };
+        await browser.storage.local.set({ user });
+        document.getElementById('message').innerText = 'UUID Set!';
+        setTimeout(() => document.getElementById('message').innerText = '', 2000); // Remove the message after 2 seconds
+    }
+}
